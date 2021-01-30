f4882ac13707e878c29920ecb9ab0ad38029781a
albert <albert@alberts-MacBook-Pro.local>
2021-01-30 23:11:35 +0100

    d

---
+++ src/pages/reports/Chart.tsx
@@ -0,0 +1,30 @@
+import React, { FunctionComponent } from 'react'
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts'
+
+type ChartProps = {
+  data: any
+}
+
+export const Chart: FunctionComponent<ChartProps> = ({data}) => {
+  return (
+    <div>
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="linear" dataKey="aggregated" stroke="#8884d8" activeDot={{ r: 2 }} dot={false} />
+      </LineChart>
+    </div>
+  )
+}
--- src/pages/reports/Reports.tsx
+++ src/pages/reports/Reports.tsx
@@ -9,35 +9,96 @@ import { Title } from '../../brewery/title/Title'
 import { Page } from '../../brewery/page/Page'
 import { urls } from '../../config'
 import { UrlParamTypes } from '../../types/common'
+import { Chart } from './Chart'
+
+const ChartWrapper = styled.div`
+  margin-bottom: 40px;
+`
+
+const Year = styled.div`
+  font-weight: bold;
+`
+
+type DataProps = {
+  year: any
+}
 
 export const Reports = () => {
-  const [data, setData] = useState([])
+  const [data2020, setData2020] = useState([])
+  const [data2019, setData2019] = useState([])
+  const [data2018, setData2018] = useState([])
+  const [data2017, setData2017] = useState([])
+  const [data2016, setData2016] = useState([])
+  const [data2015, setData2015] = useState([])
+  const [data2014, setData2014] = useState([])
+  const [data2013, setData2013] = useState([])
+  const [data2012, setData2012] = useState([])
   const { code } = useParams<UrlParamTypes>()
 
-  console.log('### data', data)
+  console.log('### data2020', data2020)
+
+  const fetchData = async (year: number) => {
+    console.log('### year', year)
+    const url = `${urls.productionApi}/db_acc_transactions/chart_data?year=${year}`
+
+    try {
+      const response = await fetch(url, {
+        method: 'get',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      })
+      const responseData = await response.json()
+
+      switch(year) {
+        case 2020:
+          setData2020(responseData)
+          break;
+        case 2019:
+          setData2019(responseData)
+          break;
+        case 2018:
+          setData2018(responseData)
+          break;
+        case 2017:
+          setData2017(responseData)
+          break;
+        case 2016:
+          setData2016(responseData)
+          break;
+        case 2016:
+          setData2016(responseData)
+          break;
+        case 2015:
+          setData2015(responseData)
+          break;
+        case 2014:
+          setData2014(responseData)
+          break;
+        case 2013:
+          setData2013(responseData)
+          break;
+        case 2012:
+          setData2012(responseData)
+          break;
+        default:
+          // code block
+      }
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
 
   useEffect(() => {
-    const fetchFiles = async () => {
-      console.log('### submitForm')
-      const url = `${urls.productionApi}/db_acc_transactions/chart_data`
-  
-      try {
-        const response = await fetch(url, {
-          method: 'get',
-          headers: {
-            'Content-type': 'application/json'
-          }
-        })
-        const responseData = await response.json()
-        console.log('### responseData', responseData)
-        setData(responseData)
-        console.log('responseData', responseData)
-      } catch (error) {
-        console.log('error', error)
-      }
-    }
-
-    fetchFiles()
+    fetchData(2020)
+    fetchData(2019)
+    fetchData(2018)
+    fetchData(2017)
+    fetchData(2016)
+    fetchData(2015)
+    fetchData(2014)
+    fetchData(2013)
+    fetchData(2012)
   }, [])
 
   return (
@@ -45,21 +106,46 @@ export const Reports = () => {
       <Title>
         {`Reports ${code}`}
       </Title>
-      <LineChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 5, right: 30, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="linear" dataKey="aggregated" stroke="#8884d8" activeDot={{ r: 2 }} dot={false} />
-      </LineChart>
+      <>
+        <ChartWrapper>
+          <Year>
+            2020
+          </Year>
+          <Chart data={data2020} />
+          <Year>
+            2019
+          </Year>
+          <Chart data={data2019} />
+          <Year>
+            2018
+          </Year>
+          <Chart data={data2018} />
+          <Year>
+            2017
+          </Year>
+          <Chart data={data2017} />
+          <Year>
+            2016
+          </Year>
+          <Chart data={data2016} />
+          <Year>
+            2015
+          </Year>
+          <Chart data={data2015} />
+          <Year>
+            2014
+          </Year>
+          <Chart data={data2014} />
+          <Year>
+            2013
+          </Year>
+          <Chart data={data2013} />
+          <Year>
+            2012
+          </Year>
+          <Chart data={data2012} />
+        </ChartWrapper>
+      </>
     </Page>
   )
 }
